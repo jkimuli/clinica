f0d1bb15871a9ab3297ddea41616ae06529538fb
Julius Kimuli <jkimuli@gmail.com>
2020-06-17 20:23:40 +0300

    changing query for retrieving users

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from clinic.models import Employee
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def index(request):
-    employees = Employee.objects.all()    
+    #employees = Employee.objects.all()  
+    users = User.objects.all().select_related('employee')  
+    
     context = {
-        'employees': employees,        
+        'employees': users,        
     }
 
     return render(request,'pages/index.html', context)
