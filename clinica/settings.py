b12784fbd04cb5520c797d132020e0286b42f1b2
Julius Kimuli <jkimuli@gmail.com>
2020-06-17 21:51:16 +0300

    Testing with postgres locally

--- clinica/settings.py
+++ clinica/settings.py
@@ -108,12 +108,16 @@ WSGI_APPLICATION = 'clinica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
+DATABASES = {    
 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }    
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': 5432,
+    }   
 }
 
 
